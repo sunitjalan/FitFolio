54f11d4c23ec241f5707025d4c76d409f6797101
sunitjalan <sunitjalan@gmail.com>
2023-11-17 19:34:20 +0530

    first commit

---
+++ FitFolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitFolio
+//
+//  Created by Sunit Jalan on 17/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
